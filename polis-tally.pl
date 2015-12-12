5acca2b81bd03a33e8cb73f76517d2525c164c60
Audrey Tang <audreyt@audreyt.org>
2015-12-12 17:51:58 +0800

    * Talkto03 tally

--- polis-tally.pl
+++ polis-tally.pl
@@ -31,4 +31,22 @@ for my $idx (0..$#agree) {
 @result = sort { $b->{percentage} <=> $a->{percentage} } @result;
 my $id = 0;
 $_->{id} = ++$id for @result;
-print JSON::XS->new->pretty(1)->canonical(1)->encode(\@result);
+my $out = shift;
+unless ($out) {
+    print JSON::XS->new->pretty(1)->canonical(1)->encode(\@result);
+    exit;
+}
+{
+    my $json = $out;
+    $json =~ s/(?:\.csv|\.json)?$/.json/;
+    open my $fh, '>:utf8', $json or die $!;
+    print $fh JSON::XS->new->pretty(1)->canonical(1)->encode(\@result);
+    close $fh;
+}
+{
+    my $csv = $out;
+    $csv =~ s/(?:\.csv|\.json)?$/.csv/;
+    my @cols = sort keys %{ $result[0] };
+    my $aoa = [ \@cols, map { [ @{$_}{@cols}]  } @result ];
+    csv(in => $aoa, out => $csv);
+}
